a9c936ad799f6e46a0abceac80b0a32288ba9f2d
EC2 Default User <ec2-user@ip-172-31-30-201.ap-northeast-1.compute.internal>
2013-06-04 09:45:31 +0000

    Changed db name.

--- cgi-bin/analyze.py
+++ cgi-bin/analyze.py
@@ -20,7 +20,7 @@ def main():
     page = 1
     trialPageCnt = 10
     dispCntPerPage = 6
-    connector = MySQLdb.connect(host="localhost",db="proposal-recommender",user="root",passwd="")
+    connector = MySQLdb.connect(host="localhost",db="proposal_recommender",user="root",passwd="")
     connector.autocommit(True)
     cursor = connector.cursor()
 
